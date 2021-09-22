f0ecfeb7b0c1640d17b42bb3b4f898e7902fe7cf
juliana monteiro <juliana47monteiro@gmail.com>
2021-09-22 18:44:37 -0300

    Pastas Atendimento

---
+++ src/pages/atendimento/mesa.js
@@ -0,0 +1,17 @@
+import React from 'react';
+
+import logoPng from '../../img/jesus-logo.png'
+
+const Mesa = () => {
+	return (
+		<div>
+			<div className='nav'>
+				<figure className='logoPng'>
+					<img src={logoPng} alt='logoPng' />
+				</figure>
+			</div>
+		</div>
+	)
+}
+
+export default Mesa;
