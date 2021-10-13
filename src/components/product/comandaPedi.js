43b27c1b32fbca9236cf1a7f5a08f1e7b783f399
jenniferpessoa <jenni.pessoa@gmail.com>
2021-10-13 00:50:30 -0300

    conflitos resolvidos

--- src/components/product/comandaPedi.js
+++ src/components/product/comandaPedi.js
@@ -1,7 +1,7 @@
 import React from "react";
 
 import './itemOrder.css'
-import { orderOrdersTime, preparationTime } from '../../utils/data'
+import { preparationTime } from '../../utils/data'
 
 import ItemComanda from './itemComanda';
 
@@ -16,14 +16,14 @@ const ComandaPedi = ({ item, className, cores, handleStatus, children }) => {
     <article className={className} key={item.id+1159}>
       <section className="info-pedido">
         <div className="info-order-kitchen">
-        <i className="fas fa-receipt"><p> - {item.id}</p></i>
+        <i className="fas fa-receipt"><p> {item.id}</p></i>
         {item.status === 'ready' && <i className="far fa-hourglass"><p>{timeready}</p></i>}
         {item.status === 'delivered' && <i className="far fa-hourglass"><p>{timeready}</p></i>}
         </div>
-        <i className="far fa-calendar-check"><p> - {(item.createdAt).substr(0, 10)}</p></i>
-        <i className="far fa-clock"><p> - {(item.createdAt).substr(11, 8)}</p></i>
-        <i className="fas fa-table"><p> - {item.table}</p></i>
-        <i className="fas fa-user"><p> - {item.client_name}</p></i>
+        <i className="far fa-calendar-check"><p> {(item.createdAt).substr(0, 10)}</p></i>
+        <i className="far fa-clock"><p> {(item.createdAt).substr(11, 8)}</p></i>
+        <i className="fas fa-table"><p> {item.table}</p></i>
+        <i className="fas fa-user"><p> {item.client_name}</p></i>
       </section>
       <ul className="item-order">
         {productsOfOrder?.map(product => {
